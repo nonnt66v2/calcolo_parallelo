34842d40378b4ce2fc723808311aacfa1d076c76
nonnt66 <lorenzo.dipalo001@studenti.uniparthenope.it>
2024-04-05 10:32:16 +0200

    somma e norma vettoriale

--- somma_tra_2_vettori/cmake-build-debug/cmake_install.cmake
+++ somma_tra_2_vettori/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/nonnt66/Scaricati/uni/calcolo_parallelo/calcolo_parallelo/hello_world
+# Install script for directory: /home/nonnt66/Scaricati/uni/calcolo_parallelo/calcolo_parallelo/somma_tra_2_vettori
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/nonnt66/Scaricati/uni/calcolo_parallelo/calcolo_parallelo/hello_world/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/nonnt66/Scaricati/uni/calcolo_parallelo/calcolo_parallelo/somma_tra_2_vettori/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
