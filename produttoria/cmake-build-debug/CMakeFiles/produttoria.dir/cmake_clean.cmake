72d6cb8d70745bd291cb6ab7d8f31acf7105cf35
nonnt66v2 <91503962+nonnt66v2@users.noreply.github.com>
2024-06-02 13:12:09 +0200

    caricamento parallelizzato

---
+++ produttoria/cmake-build-debug/CMakeFiles/produttoria.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/produttoria.dir/main.c.o"
+  "CMakeFiles/produttoria.dir/main.c.o.d"
+  "produttoria"
+  "produttoria.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/produttoria.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- produttoria/main.c
+++ produttoria/main.c
@@ -18,28 +18,20 @@ int main(int argc, char **argv) {
     int **mat = (int **) calloc(N, sizeof(int *));
     int *vetA = (int *) calloc(M, sizeof(int));
     int *vetB = (int *) calloc(N, sizeof(int));
-    for (int i = 0; i < N; i++) {
-        mat[i] = (int *) calloc(M, sizeof(int));
-        for (int j = 0; j < M; j++) {
-//            mat[i][j] = (int) rand();
-            mat[i][j] = 2;
 
-        }
-    }
-    for (int i = 0; i < M; i++) {
-//        vetA[i] = (int) rand();
-        vetA[i] = 2;
-    }
-    for (int i = 0; i < N; i++) {
-        vetB[i] = (int) rand();
-    }
+    double t0, t1, t2, t3;
     int nloc = N / numT;
     int i, j;
     int r;
     int step = 0;
     int *R = (int *) calloc(M, sizeof(int));
     int somma = 0;
-#pragma omp parallel private(i, j, step) shared(mat, vetA, alpha, beta, nloc, N, M, R) num_threads(numT) reduction(+:somma)
+    int indice = 0;
+    t0 = omp_get_wtime();
+    for (int k = 0; k < N; k++) {
+        mat[k] = (int *) calloc(M, sizeof(int));
+    }
+#pragma omp parallel private(i, j, step, indice) shared(mat, vetA, alpha, beta, nloc, N, M, R) num_threads(numT)
     {
 
         r = N % numT;
@@ -50,21 +42,45 @@ int main(int argc, char **argv) {
         } else {
             step = r;
         }
-        int id = omp_get_thread_num();
-        for (i = id * (N / numT); i < ((id + 1) * (N / numT)); i++) {
+        t2 = omp_get_wtime();
+        for (i = 0; i < nloc; i++) {
+            indice = i + nloc * omp_get_thread_num() + step;
+//        vetA[i] = (int) rand();
+            vetA[indice] = 1;
+        }
+        for (i = 0; i < nloc; i++) {
+            indice = i + nloc * omp_get_thread_num() + step;
+            vetB[indice] = 1;
+        }
+        for (i = 0; i < nloc; i++) {
+            indice = i + nloc * omp_get_thread_num() + step;
             for (j = 0; j < M; j++) {
-                vetA[i] *= mat[i][j];
-                vetB[i] *= mat[j][i];
+//            mat[i][j] = (int) rand();
+                mat[indice][j] = 1;
             }
         }
-        for (i = id * (N / numT); i < ((id + 1) * (N / numT)); i++) {
-            R[i] = alpha * vetA[i] * beta + vetB[i] * alpha;
+        t3 = omp_get_wtime();
+        printf("indice [%d] thread [%d] -> %.16lf\n", indice, omp_get_thread_num(), t3 - t2);
+        indice = 0;
+        for (i = 0; i < nloc; i++) {
+            for (j = 0; j < M; j++) {
+                indice = i + nloc * omp_get_thread_num() + step;
+                vetA[indice] *= mat[indice][j];
+                vetB[indice] *= mat[j][indice];
+            }
+        }
+        for (i = 0; i < nloc; i++) {
+            for (j = 0; j < M; j++) {
+                indice = i + nloc * omp_get_thread_num() + step;
+                R[indice] = alpha * mat[indice][j] * beta + vetB[indice] * alpha;
+            }
         }
 
     }
+    t1 = omp_get_wtime();
+    //decommentare per stampa
     for (int k = 0; k < M; ++k) {
-        printf("vet[%d] = %d\n", k, vetA[k]);
+        printf("vet[%d] = %d\n", k, R[k]);
     }
-
-
+    printf("il calcolo ha richiesto [%.16lf] secondi\n", t1 - t0);
 }
