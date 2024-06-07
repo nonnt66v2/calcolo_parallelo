bf5211452c4ccaaf760d1fb207ba8a105a638dc6
nonnt66v2 <91503962+nonnt66v2@users.noreply.github.com>
2024-06-07 19:50:42 +0200

    simulazione

--- simulazione_esame2/main.c
+++ simulazione_esame2/main.c
@@ -5,6 +5,57 @@
 #include "stdlib.h"
 #include "malloc.h"
 
+
+void allocationArray(int** array, int N) {
+    *array = (int*)calloc(N, sizeof(int));
+}
+
+void fillArray(int** array, int N) {
+    for (int i = 0; i < N; i++)
+        (*array)[i] = rand() % 11;
+}
+
+void printArray(int* array, int N, char* name) {
+    printf("\nVector %s:\n", name);
+    for (int i = 0; i < N; i++) {
+        printf("%d    ", array[i]);
+    }
+    printf("\n");
+}
+
+void deallocaVet(int** array,int N) {
+    free(*array);
+}
+
+void deallocaMat(int*** matrix, int N) {
+    for (int i = 0; i < N; i++)
+        free((*matrix)[i]);
+    free(*matrix);
+}
+
+void initializeMatrix(int*** matrix, int N) {
+    *matrix = (int**)calloc(N, sizeof(int*));
+    for (size_t i = 0; i < N; ++i) {
+        (*matrix)[i] = (int*)calloc(N, sizeof(int));
+    }
+
+    // Inizializzazione della matrice
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            (*matrix)[i][j] = (i + 1) * (j + 1);
+        }
+    }
+}
+
+void printMatrix(int** matrix, int N, char* name) {
+    printf("\nMatrix %s:\n", name);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
 int main(int argc, char **argv) {
     if (argc < 3) {
         printf("fornire in input rispettivamente dimensione di matrice, scalari e num_threads\n");
@@ -60,15 +111,20 @@ int main(int argc, char **argv) {
 
     t1 = omp_get_wtime();
     //decommentare per stampa
-    for (int k = 0; k < N; k++) {
-        for (int i=0;i<N;i++) {
-            printf("%d ",matA[k][i]);
-        }
-        printf("\n");
-    }
-    for (int l = 0; l < N; ++l) {
-        printf("%d ",R[k]);
+//    for (int k = 0; k < N; k++) {
+//        for (int i=0;i<N;i++) {
+//            printf("%d ",matA[k][i]);
+//        }
+//        printf("\n");
+//    }
+//    for (int l = 0; l < N; ++l) {
+//        printf("%d ",R[k]);
+//
+//    }
+    deallocaMat(&matA,N);
+    deallocaMat(&matB,N);
+    deallocaVet(&vetA,N);
+    deallocaVet(&vetB,N);
 
-    }
     printf("il calcolo ha richiesto [%.16lf] secondi\n", t1 - t0);
 }
